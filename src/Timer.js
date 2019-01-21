56fdf1b4bb4bc57878c24e24920795e2ded03916
Zac B <zacbennett0@gmail.com>
2019-01-21 15:06:42 -0800

    added pause button

--- src/Timer.js
+++ src/Timer.js
@@ -4,38 +4,51 @@ class Timer extends Component {
   constructor(props) {
     super(props)
 
-    this.state = { minutes: 0, seconds: 0 };
-    this.handleClick = this.handleClick.bind(this)
+    this.state = { minutes: 0, seconds: 0, timerId: 0};
+    this.handleStart = this.handleStart.bind(this)
+    this.handlePause = this.handlePause.bind(this)
     this.handleTimer = this.handleTimer.bind(this)
+
+    
   }
 
-  handleTimer(){
+  handleTimer() {
     let thisClass = this;
 
-    setInterval(function(){ 
-      if(thisClass.state.seconds === 0){
-        thisClass.setState((state) => {
-          return {minutes: state.minutes - 1,seconds: 59};
-        });
-      }else{
-        thisClass.setState((state) => {
-          return {seconds: state.seconds - 1};
-        });
-      }
-    }, 1000, thisClass);
+    this.setState((state) => {
+      return {timerId: setInterval(function(){ 
+        if(thisClass.state.seconds === 0){
+          thisClass.setState((state) => {
+            return {minutes: state.minutes - 1,seconds: 59};
+          });
+        }else{
+          thisClass.setState((state) => {
+            return {seconds: state.seconds - 1};
+          });
+        }
+      }, 1000, thisClass)}
+    }) 
 
-    // timer();
 
-    // if(this.state.minutes === 0 && this.state.seconds === 0){
-    //     clearInterval(timer)
-    // }
   }
 
-  handleClick(){
-    // console.log(typeof this.state.minutes)
+  componentDidUpdate(){
+    if(this.state.minutes === 0 && this.state.seconds === 0){
+      clearInterval(this.state.timerId)
+    }
+  }
+
+  handleStart(){
     this.setState((state) => {
-      return {minutes: state.minutes + 5};
+      // return {minutes: state.minutes + 1};
+      return {seconds: 10};
     },this.handleTimer());
+  }
+  handlePause(){
+    // console.log(typeof this.state.minutes)
+    console.log(':)')
+    clearInterval(this.state.timerId)
+
   }
 
   
@@ -46,7 +59,8 @@ class Timer extends Component {
         <h1>
           {this.state.minutes}:{this.state.seconds}
         </h1>
-        <button onClick={this.handleClick}>Click!</button>
+        <button onClick={this.handleStart}>Start!</button>
+        <button onClick={this.handlePause}>Pause!</button>
       </div>
     );
   }
---
+++ todo.md
@@ -0,0 +1,13 @@
+timer for work!
+pause button!
+
+timer for break
+pause button 
+
+make it so that timer has two digits for minutes and seconds
+
+create tags
+assign tags to timer
+show stats for tags
+styling
+
